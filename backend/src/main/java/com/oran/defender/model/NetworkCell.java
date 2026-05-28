cfae0c6dd047f23cb49f009079e9487190383748
Yufeng Wang <DamZoe@users.noreply.github.com>
2026-05-28 14:53:31 +0100

    Fix JPA entity identifiers

--- backend/src/main/java/com/oran/defender/model/NetworkCell.java
+++ backend/src/main/java/com/oran/defender/model/NetworkCell.java
@@ -1,7 +1,17 @@
 package com.oran.defender.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class NetworkCell {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
 }
